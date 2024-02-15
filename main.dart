c057a95596138c1e1a07028c49fe2cbb4d135329
sajibNE <mdsajib-07-2019718002@ne.du.ac.bd>
2024-02-15 07:28:27 +0600

    Added a function to check whether two objects are equal.

--- main.dart
+++ main.dart
@@ -7,6 +7,17 @@ Students copyObject(Students stu1) {
   return stu2;
 }
 
+bool checkEqual(Object obj1, Object obj2) {
+  if (obj1 is Students && obj2 is Students) {
+    if ((obj1.name == obj2.name) &&
+        (obj1.id == obj1.id) &&
+        (obj1.roll == obj2.roll) &&
+        (obj1.age == obj1.age)) return true;
+  }
+
+  return false;
+}
+
 void main() {
   Students stu1 = Students(name: "Sajib", id: "xxx199", roll: 199, age: 24);
 
@@ -16,4 +27,8 @@ void main() {
   print(newStu.id);
   print(newStu.roll);
   print(newStu.age);
+
+  //checking whether two object are same.
+
+  print(checkEqual(stu1, newStu));
 }
